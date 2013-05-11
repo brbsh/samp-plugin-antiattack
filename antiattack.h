903a44d817e8ffe94b160617d4073e947d2bab70
Vladislav <bjiadokc@gmail.com>
2013-05-11 15:24:21 +0600

    Removed some stuff from C include

--- antiattack.h
+++ antiattack.h
@@ -6,28 +6,28 @@
 #include "SDK/plugincommon.h"
 
 #if (defined(WIN32) || defined(_WIN32) || defined(_WIN64))
-	#include "windows.h"
+	#include <windows.h>
 #else
-	#include "pthread.h"
+	#include <pthread.h>
 #endif
 
-#include "time.h"
-#include "sstream"
-#include "fstream"
-#include "iostream"
-#include "vector"
-#include "queue"
-#include "list"
-#include "string.h"
+#include <time.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <list>
+#include <string>
 
 #if defined(LINUX) || defined(FREEBSD) || defined(__FreeBSD__) || defined(__OpenBSD__)
-	#include "unistd.h"
-	#include "stdarg.h"
-	#include "complex"
-	#include "algorithm"
+	#include <unistd.h>
+	#include <stdarg.h>
+	#include <complex>
+	#include <algorithm>
 #endif
 
-#define arguments(a) !(params[0] != (a << 2))
+#define arguments(a) \
+	!(params[0] != (a << 2))
 
 typedef void (*logprintf_t)(char* format, ...);
 
@@ -38,30 +38,12 @@ typedef void (*logprintf_t)(char* format, ...);
 	typedef unsigned long DWORD;
 #endif
 
-#if !defined NULL
-	#define NULL 0
-#endif
-
-
-//using namespace std;
-using std::string;
-using std::stringstream;
-using std::list;
-using std::ofstream;
-using std::basic_istringstream;
-using std::queue;
-using std::vector;
-
-#if defined(LINUX) || defined(FREEBSD) || defined(__FreeBSD__) || defined(__OpenBSD__)
-	using std::complex;
-#endif
-
 std::string stringvprintf(const char *format, va_list args)
 {
-	int length = vsnprintf(NULL,0,format,args);
-	char *chars = new char[length + 1];
+	int length = vsnprintf(NULL, 0, format, args);
+	char *chars = new char[++length];
 
-	length = vsnprintf(chars, length + 1, format, args);
+	length = vsnprintf(chars, length, format, args);
 	std::string result(chars);
 	delete chars;
 
